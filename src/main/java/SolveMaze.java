5d6931be9b4ba19b61b7b37e26dd6b21093d2f12
pacheco9 <42983426+pacheco9@users.noreply.github.com>
2018-09-11 18:26:26 -0500

    seventh draft

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,18 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                if (!maze.canMove()) {
+                    maze.turnRight();
+                    if (!maze.canMove()) {
+                        maze.move();
+                    } else if (!maze.canMove()) {
+                        maze.turnLeft();
+                        maze.turnLeft();
+                        maze.move();
+                    }
+                }
+            }
         }
 
         if (maze.isFinished()) {
